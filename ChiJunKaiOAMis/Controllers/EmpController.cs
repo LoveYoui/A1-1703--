00d96ab1a5b70db6477678b38e47acc014e601ab
LoveYoui <2580302344@qq.com>
2019-01-19 15:47:19 +0800

    A1-1703

---
+++ ChiJunKaiOAMis/Controllers/EmpController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ChiJunKaiOAMis.Models;
+
+namespace ChiJunKaiOAMis.Controllers
+{
+    public class EmpController : Controller
+    {
+        OADBEntities db = new OADBEntities();
+        // GET: Emp
+        public ActionResult Index()
+        {
+            ViewBag.name = db.Dep.ToList();
+            return View();
+        }
+        public ActionResult Search(int limit=10, int page=1) {
+            IQueryable<View_Employees> res = db.View_Employees.Where(p=>true);
+            String realName = Request.Params["realName"];
+            String name = Request.Params["name"];
+            String sex = Request.Params["sex"];
+            if (!String.IsNullOrWhiteSpace(realName))
+                res = res.Where(p => p.RealName.Contains(realName));
+            if (!String.IsNullOrWhiteSpace(name))
+                res = res.Where(p => p.Name.Contains(name));
+            if (!String.IsNullOrWhiteSpace(sex))
+                res = res.Where(p => p.Sex.Contains(sex));
+            var list = res.OrderBy(p=>p.Name).Skip((page-1)*limit).Take(limit).ToList();
+            var count = res.Count();
+            return Content(Pager.PagedData(list,count));
+        }
+        public ActionResult Del(Guid id)
+        {
+            var a = db.Employees.SingleOrDefault(p => p.UserID == id);
+            if (a == null)
+            {
+                return Json(new { success = false, msg = "请选择要删除的员工" });
+            }
+            else {
+                db.Employees.Remove(a);
+                db.SaveChanges();
+                return Json(new { success = true, msg = "删除成功" });
+            }
+        }
+    }
+}
---
+++ ChiJunKaiOAMis/Models/Pager.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace ChiJunKaiOAMis.Models
+{
+    public class Pager
+    {
+        public static String PagedData<T>(List<T> list, int count, String dateTimeFormat = "yyyy-MM-dd HH:mm:ss")
+        {
+            StringBuilder strJson = new StringBuilder();
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            settings.DateFormatHandling = Newtonsoft.Json.DateFormatHandling.MicrosoftDateFormat;
+            settings.DateFormatString = dateTimeFormat;
+            settings.MaxDepth = 1; //设置序列化的最大层数  
+            settings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            settings.NullValueHandling = NullValueHandling.Ignore;
+            foreach (var item in list)
+            {
+                strJson.Append(JsonConvert.SerializeObject(item, settings) + ",");
+            }
+            return "{\"code\":0,\"msg\":\"\",\"count\":" + count + ",\"data\":[" + strJson.ToString().TrimEnd(',') + "]}";
+        }
+    }
+}
